b63b7ab5157514f49d6a404989fdd9758a9fe907
Vahid <vpiroozbakht@gmail.com>
2020-12-03 14:25:26 +0200

    remove extra loop and sort

--- min_deletions_to_make_frequency_of_each_letter_unique/main.go
+++ min_deletions_to_make_frequency_of_each_letter_unique/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func main() {
@@ -19,23 +18,10 @@ func solution(s string) int {
 		listOfChar[char] = listOfChar[char] + 1
 	}
 
-	// if len(listOfChar) <= 1 {
-	// 	return 0
-	// }
-	// if len(listOfChar) == 2 {
-	// 	return 1
-	// }
-
-	var list []int
-	for _, val := range listOfChar {
-		list = append(list, val)
-	}
-	sort.Sort(sort.Reverse(sort.IntSlice(list)))
-
 	count := 0
 	var listTarget []int
-	for i := 0; i < len(list); i++ {
-		a := list[i]
+	for _, val := range listOfChar {
+		a := val
 		if !isInList(a, listTarget) {
 			listTarget = append(listTarget, a)
 		} else {
@@ -53,6 +39,7 @@ func solution(s string) int {
 			}
 		}
 	}
+
 	return count
 }
 
